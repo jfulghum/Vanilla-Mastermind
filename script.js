8e18c094d1b09ca8f75c52cd98bb430806645f77
johanna fulghum <johanna.fulghum@gmail.com>
2017-11-07 20:52:57 -0500

    Empties rerender properly

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 var currentColor = null
+var wrongGuesses = [];
+var code = [];
+var guess = [];
+
+var COLORS = {
+  'turquoise' : "rgb(64, 224, 208)",
+  'black' : "rgb(0, 0, 0)",
+  'purple' : "rgb(128, 0, 128)",
+  'orange' : "rgb(255, 165, 0)",
+  'blue' : "rgb(0, 0, 255)",
+  'red' : "rgb(255, 0, 0)",
+  'green' : "rgb(0, 255, 0)",
+  'yellow' : "rgb(64, 224, 208)"
+}
 
 function canvasMaker(id,color){
     var c = document.getElementById(id);
@@ -19,14 +33,50 @@ canvasMaker("canv6", "black")
 canvasMaker("canv7", "purple")
 canvasMaker("canv8", "orange")
 
+function createRow() {
+  var child = document.createElement("ul");
+  child.innerHTML =
+  '<li class="empty"></li>' +
+  '<li class="empty"></li>' +
+  '<li class="empty"></li>' +
+  '<li class="empty"></li>';
+  return child;
+}
 
+generateCompCode();
+renderEmpties();
+
+function renderEmpties() {
+  var empties = document.querySelector("#empties-placeholder");
+  empties.innerHTML = "";
+  for (var i = 0; i < 9-wrongGuesses.length; ++i) {
+    empties.appendChild(createRow());
+  }
+}
+
+function renderWrongGuesses() {
+  var pastGuesses = document.querySelector("#past-guesses");
+  pastGuesses.innerHTML = "";
+  for (var i = 0; i < wrongGuesses.length; ++i) {
+    var row = createRow();
+    for (var k = 0; k < row.children.length; ++k) {
+      row.children[k].classList.add(wrongGuesses[i][k]);
+    }
+    pastGuesses.appendChild(row);
+  }
+}
+
+function render() {
+  renderWrongGuesses();
+  renderEmpties();
+}
 
 function changeCursor(el){
     var mywindow = document.getElementsByTagName('html');
     currentColor = el.getContext("2d").fillStyle;
 
     var img = el.toDataURL()
-    mywindow[0].setAttribute('style', 'cursor: url('+img+'),auto;');
+    mywindow[0].setAttribute('style', 'cursor: url('+img+') 20 20,auto;');
 }
 
 for (var i = 0; i < 4; i++){
@@ -54,6 +104,8 @@ function submitEvent(){
   }
 }
 
+
+
 var myBtn = document.getElementById("submitButton");
 myBtn.addEventListener('click', function (event){
   console.log(correctSubmit());
@@ -61,8 +113,14 @@ myBtn.addEventListener('click', function (event){
     firstRow = document.getElementById("first-row").children;
     for (var i = 0; i < firstRow.length; i++){
       rgb = firstRow[i].style.backgroundColor;
+      firstRow[i].style.backgroundColor = "";
+      firstRow[i].style.backgroundImage = "url('wood_background2.jpg')";
       guess.push(parseCOLOR(COLORS, rgb));
     }
+    codeCheck();
+    wrongGuesses.push(guess);
+    guess = [];
+    render();
   }
 });
 
@@ -70,16 +128,7 @@ function changeColor(el){
   el.style.background = currentColor;
 }
 
-var COLORS = {
-  'turquoise' : "rgb(64, 224, 208)",
-  'black' : "rgb(0, 0, 0)",
-  'purple' : "rgb(128, 0, 128)",
-  'orange' : "rgb(255, 165, 0)",
-  'blue' : "rgb(0, 0, 255)",
-  'red' : "rgb(255, 0, 0)",
-  'green' : "rgb(0, 255, 0)",
-  'yellow' : "rgb(64, 224, 208)"
-}
+
 
 function parseCOLOR(object, rgb){
   return Object.keys(object).find(key => object[key] === rgb);
@@ -90,25 +139,25 @@ function generateCompCode(){
     var rand = Math.floor(Math.random() * Object.keys(COLORS).length);
     code.push(Object.keys(COLORS)[rand]);
   }
+  console.log("Current code is", code);
   return code;
 }
 
-var code = [];
-var guess = [];
+
 
 function codeCheck(){
-  var exact_count = 0;
-  var near_count = 0;
-  for (var i = 0; i < code.length; i ++){
-    if (code[i] === guess[i]){
-      code[i] = NaN;
-      exact_count ++;
-      guess[i] = NaN;
-    }
-    if (code.includes(guess[i])){
-      near_count ++
-    }
-  }
-  near_count = Math.abs(near_count - exact_count)
-  return [exact_count, near_count];
+  // var exact_count = 0;
+  // var near_count = 0;
+  // for (var i = 0; i < code.length; i ++){
+  //   if (code[i] === guess[i]){
+  //     code[i] = NaN;
+  //     exact_count ++;
+  //     guess[i] = NaN;
+  //   }
+  //   if (code.includes(guess[i])){
+  //     near_count ++
+  //   }
+  // }
+  // near_count = Math.abs(near_count - exact_count)
+  // return [exact_count, near_count];
 }
